e1866e51a6c18ee15c597af409c248b26357bc68
Catlike14 <alfonsomattia@gmail.com>
2025-08-01 08:40:51 +0200

    Fixed syntax error

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ export interface InstallExtensionOptions {
   session?: Session;
 }
 
-type ExtensionApi = Pick<Session, 'getAllExtensions', 'removeListener', 'removeExtension', 'loadExtension', 'on'>;
+type ExtensionApi = Pick<Session, 'getAllExtensions' | 'removeListener' | 'removeExtension' | 'loadExtension' | 'on'>;
 
 /**
  * @param extensionReference Extension or extensions to install
